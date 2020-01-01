c8ad4bc66aab919aad39a5a269ab86c973017b78
Kevin Liu <kliu0862@bths.edu>
2020-01-01 18:11:16 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="assets/css/index.css">
+    <meta charset="utf-8" />
+    <title>KevIndex</title>
+    <style>
+        * 
+        {
+            background-color: #f8f8f8;
+            color: #1c1c1c;
+            font-family: Arial, Helvetica, sans-serif;
+            text-align: center;
+            margin: 0 auto;
+        }
+
+        h3
+        {
+            margin: 1em;
+        }
+
+        #pageContent
+        {
+            margin: 2em;
+            padding: 1em;
+            border: .5cm black solid;
+        }
+    </style>
+</head>
+<body>
+    <h3>KevIndex</h3>
+
+    <div id = "pageContent">
+        Coming Soon...
+    </div>
+</body>
+</html>
